d783b7008e4269c4cb627e2d618860f91db896f5
FlatTireTwitch <alan@valerio.nyc>
2023-02-02 02:23:10 -0500

    Added hours section

--- main.js
+++ main.js
@@ -22,3 +22,25 @@ window.onscroll = function() {
         header.style.boxShadow = "0 0 0px black"
     }
 }
+
+// get current date and day of week
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const today = new Date()
+const monthDay = today.toLocaleDateString("en-US", {month: "2-digit", day: "2-digit"})
+const dayOfWeek = daysOfWeek[today.getUTCDay()]
+var datesOfWeek = []
+
+for (let i = 0; i < 7; i++) {
+    let date = new Date()
+    date.setDate(date.getDate() - date.getDay() + i)
+    let month = date.getMonth() + 1 // 0-indexed, so add 1
+    let day = date.getDate()
+    document.querySelectorAll(`.${daysOfWeek[i]}.date`)[0].textContent = month + "/" + day
+    document.querySelectorAll(`.${daysOfWeek[i]}.date`)[1].textContent = month + "/" + day
+    datesOfWeek.push(month + "/" + day)
+}
+
+const dayEls = document.getElementsByClassName(dayOfWeek)
+for (let i = 0; i < dayEls.length; i++) {
+    dayEls[i].style.fontWeight = "900"
+}
