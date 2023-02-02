d881beb8c9b0c969afeea5abc42211b2ce3ef73e
FlatTireTwitch <alan@valerio.nyc>
2023-02-02 00:40:50 -0500

    Better banner system

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-// hide banner if empty
+// hide banner if empty or set to null or none
 var bannerText = document.getElementById('banner')
+var bannerWrapper = document.getElementById('banner-wrapper')
 
-if (!bannerText.textContent.trim().length) {
+if (bannerText.textContent.trim().toLowerCase() == "none" || bannerText.textContent.trim().toLocaleLowerCase() == "null" || !bannerText.textContent.trim().length) {
     console.log("Banner is empty, hiding element")
-    bannerText.parentNode.removeChild(bannerText)
+    bannerWrapper.style.display = 'none'
+}
+
+// hide banner when clicked
+function hideBanner() {
+    bannerWrapper.style.display = 'none'
 }
 
 // shadow under header when scrolled
+var headerEl = document.getElementById("header")
+
 window.onscroll = function() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.getElementById("header").style.boxShadow = "0 0 20px #19191d"
+        header.style.boxShadow = "0 0 20px black"
     } else {
-        document.getElementById("header").style.boxShadow = "0 0 0px #19191d"
+        header.style.boxShadow = "0 0 0px black"
     }
 }
