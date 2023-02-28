8c5787610958a1ce6d4403833dd516e93beaa4e2
FlatTireTwitch <alan@valerio.nyc>
2023-02-28 18:32:11 -0500

    Cleaned up hours and made the json more intuitive

--- scripts/hours.js
+++ scripts/hours.js
@@ -8,23 +8,40 @@ function drawHours(hours) {
         tempDate.setDate(tempDate.getDate() + i - 1)
         let month = tempDate.toLocaleDateString("en-US", {month: "2-digit"})
         let date = tempDate.toLocaleDateString("en-US", {day: "2-digit"})
-        let day = daysOfWeek[tempDate.getDay()]
+        let weekDayNum = tempDate.getDay()
+        let day = daysOfWeek[weekDayNum]
         document.querySelectorAll(`.Day${i+1}.date`).forEach(item => {item.textContent = month + "/" + date})
         document.querySelectorAll(`.Day${i+1}.day`).forEach(item => {item.textContent = day})
-        let storeTimes = hours.storeHours[daysOfWeek.indexOf(day)].padStart(6).padEnd(13)
-        let deliveryTimes = hours.deliveryHours[daysOfWeek.indexOf(day)].padStart(6).padEnd(13)
+        let storeTimes = hours.storeHours[weekDayNum]
+        let deliveryTimes = hours.deliveryHours[weekDayNum]
         if (hours[`${month}-${date}`] != undefined) {
-            storeTimes = hours[`${month}-${date}`].store.padStart(6).padEnd(13)
-            deliveryTimes = hours[`${month}-${date}`].delivery.padStart(6).padEnd(13)
             if (hours[`${month}-${date}`].closed) {
                 document.querySelectorAll(`.Day${i+1}.day-row`).forEach(item => {item.classList.add("closed-row")})
+                storeTimes = false
+                deliveryTimes = false
+            } else {
+                if (hours[`${month}-${date}`].store != undefined && hours[`${month}-${date}`].store !== true) {
+                    storeTimes = hours[`${month}-${date}`].store
+                }
+                if (hours[`${month}-${date}`].delivery != undefined && hours[`${month}-${date}`].delivery !== true) {
+                    deliveryTimes = hours[`${month}-${date}`].delivery
+                }
             }
         }
-        document.getElementById(`store-day${i+1}-hours`).textContent = storeTimes
-        document.getElementById(`delivery-day${i+1}-hours`).textContent = deliveryTimes
-        if (deliveryTimes == "No Deliveries" && i != 1) {
-            document.querySelectorAll(`.Day${i+1}.day-row`)[1].classList.add("closed-row")
+        if (!storeTimes) {
+            storeTimes = hours.storeClosed
+            if (i != 1) {
+                document.querySelectorAll(`.Day${i+1}.day-row`)[0].classList.add("closed-row")
+            }
         }
+        if (!deliveryTimes) {
+            deliveryTimes = hours.deliveryClosed
+            if (i != 1) {
+                document.querySelectorAll(`.Day${i+1}.day-row`)[1].classList.add("closed-row")
+            }
+        }
+        document.getElementById(`store-day${i+1}-hours`).textContent = storeTimes.padEnd(hours.maxChars)
+        document.getElementById(`delivery-day${i+1}-hours`).textContent = deliveryTimes.padEnd(hours.maxChars)
         if (i == 1) {
             document.querySelectorAll(`.Day${i+1}.day-row`).forEach(item => {item.classList.add("highlighted-row")})
         }
