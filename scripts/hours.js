ec66269f5eaaaea4180fdeb60df34a0e18ff5e93
FlatTireTwitch <alan@valerio.nyc>
2023-02-28 03:32:17 -0500

    Made store hours MUCH more dynamic

--- scripts/hours.js
+++ scripts/hours.js
@@ -2,17 +2,32 @@ const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const today = new Date()
 const dayOfWeek = daysOfWeek[today.getDay()]
 
-const dayEls = document.getElementsByClassName(dayOfWeek)
-for (let i = 0; i < dayEls.length; i++) {
-    dayEls[i].style.fontWeight = "600"
-    dayEls[i].style.color = "#19191d"
-    dayEls[i].style.background = "#d8d0c3"
-}
-
 function drawHours(hours) {
-    for (let i = 0; i < daysOfWeek.length; i++) {
-        document.getElementById(`store-${daysOfWeek[i].toLowerCase()}-hours`).textContent = hours.storeHours[i]
-        document.getElementById(`delivery-${daysOfWeek[i].toLowerCase()}-hours`).textContent = hours.deliveryHours[i]
+    for (let i = 0; i < 7; i++) {
+        let tempDate = new Date()
+        tempDate.setDate(tempDate.getDate() + i - 1)
+        let month = tempDate.toLocaleDateString("en-US", {month: "2-digit"})
+        let date = tempDate.toLocaleDateString("en-US", {day: "2-digit"})
+        let day = daysOfWeek[tempDate.getDay()]
+        document.querySelectorAll(`.Day${i+1}.date`).forEach(item => {item.textContent = month + "/" + date})
+        document.querySelectorAll(`.Day${i+1}.day`).forEach(item => {item.textContent = day})
+        let storeTimes = hours.storeHours[daysOfWeek.indexOf(day)].padStart(6).padEnd(13)
+        let deliveryTimes = hours.deliveryHours[daysOfWeek.indexOf(day)].padStart(6).padEnd(13)
+        if (hours[`${month}-${date}`] != undefined) {
+            storeTimes = hours[`${month}-${date}`].store.padStart(6).padEnd(13)
+            deliveryTimes = hours[`${month}-${date}`].delivery.padStart(6).padEnd(13)
+            if (hours[`${month}-${date}`].closed) {
+                document.querySelectorAll(`.Day${i+1}.day-row`).forEach(item => {item.classList.add("closed-row")})
+            }
+        }
+        document.getElementById(`store-day${i+1}-hours`).textContent = storeTimes
+        document.getElementById(`delivery-day${i+1}-hours`).textContent = deliveryTimes
+        if (deliveryTimes == "No Deliveries" && i != 1) {
+            document.querySelectorAll(`.Day${i+1}.day-row`)[1].classList.add("closed-row")
+        }
+        if (i == 1) {
+            document.querySelectorAll(`.Day${i+1}.day-row`).forEach(item => {item.classList.add("highlighted-row")})
+        }
     }
 }
 
@@ -20,17 +35,3 @@ fetch('hours.json').then(h => h.text()).then(h => {
     const json = JSON.parse(h)
     drawHours(json)
 })
-
-// const month = today.toLocaleDateString("en-US", {month: "2-digit"})
-// const date = today.toLocaleDateString("en-US", {day: "2-digit"})
-// var datesOfWeek = []
-
-// for (let i = 0; i < 7; i++) {
-//     let date = new Date()
-//     date.setDate(date.getDate() - date.getDay() + i)
-//     let month = date.getMonth() + 1 // 0-indexed, so add 1
-//     let day = date.getDate()
-//     // document.querySelectorAll(`.${daysOfWeek[i]}.date`)[0].textContent = month + "/" + day
-//     // document.querySelectorAll(`.${daysOfWeek[i]}.date`)[1].textContent = month + "/" + day
-//     datesOfWeek.push(month + "/" + day)
-// }
