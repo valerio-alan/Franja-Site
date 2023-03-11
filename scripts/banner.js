629219d18d6910d6dbee450e19f4ef8608135391
FlatTireTwitch <alan@valerio.nyc>
2023-03-11 13:58:11 -0500

    Fixed intro z-index and added banner anim

--- scripts/banner.js
+++ scripts/banner.js
@@ -6,18 +6,19 @@ const bannerWrapper = document.getElementById('banner-wrapper')
 fetch('https://franjaapi-1-v0251088.deta.app/banner').then(b => b.text()).then(b => {
     const banner = JSON.parse(b)
     if (banner.text) {
-        bannerText.innerHTML = banner.text
-        bannerWrapper.style.display = 'flex'
-        if (banner.link) {
-            bannerLink.setAttribute("href", banner.link)
-            bannerLink.setAttribute("target", "_blank")
-        }
-        console.log("👁 | Banner has been loaded")
+        setTimeout(() => {
+            bannerText.innerHTML = banner.text
+            bannerWrapper.style.display = 'flex'
+            if (banner.link) {
+                bannerLink.setAttribute("href", banner.link)
+                bannerLink.setAttribute("target", "_blank")
+            }
+            console.log("👁 | Banner has been loaded")
+        }, 1500)
     } else {
         console.log("👁 | Banner is empty, keeping it hidden")
     }
-}).catch(error => {
-    console.error('Error:', error)
+}).catch(() => {
     console.error("👁 | Banner is broken, keeping it hidden")
   })
 
